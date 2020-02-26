4728996c1142a7bc1941cb7a1f97f30682d46c1d
Ahmed Lotfy <ahmed.lotfy@badrit.com>
2020-02-26 12:13:12 +0200

    Add exist code

---
+++ WidgetDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by Ahmed Lotfy on 2/19/20.
+//  Copyright © 2020 Ahmed Lotfy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
