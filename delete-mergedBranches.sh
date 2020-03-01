490bbe6792499b2175e1fd2852328468f79ce193
Ahmed Lotfy <ahmed.lotfy@badrit.com>
2020-03-01 10:37:38 +0200

    Update script

--- delete-mergedBranches.sh
+++ delete-mergedBranches.sh
@@ -1,13 +1,21 @@
 #!/bin/bash 
 
 deleteAll() {
-   for Branch in $1 
+  Branches=("$@")
+   for Branch in $Branches 
     do
+      echo $Branch
       git push origin --delete $Branch
     done
  }
 
 IFS=$'\n' read -rd '' -a Merged_Branches_List <$1
+
+if [ ${#Merged_Branches_List[@]} -eq 0 ]; then
+    echo "Warning: empty branches."
+    echo Exit
+    exit 1
+fi
 
 echo -e This bash script will delete all attached branches: 
 
@@ -18,7 +26,7 @@ done
 
 read -r -p "Are you sure? [yes/no]" response
  if [[ $response =~ ^(yes) ]]; then
-    deleteAll ${Merged_Branches_List[@]]}
+    deleteAll "${Merged_Branches_List[@]}"
   else
     echo Cancelled
  fi
